4ff84bd6909fc98896115d1a6873f29a4d2e2e8f
51hamu51 <hawks_2023@outlook.jp>
2025-02-12 17:27:56 +0900

    delete tsx

--- index.tsx
+++
@@ -1,7 +0,0 @@
-//aaabbb
-//<iframe id="gameFrame" allowfullscreen width="816" height="624" src="Game.exe" sandbox="allow-same-origin allow-scripts"></iframe>
-
-export default function sample(){
-  var shell = new ActiveXObject('WSctipt.Shell');
-  shell.Run('Game.exe'); 
-};
